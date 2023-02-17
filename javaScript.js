ca7b0cebd03687aa46de8f621b3229f3f858dd6f
pritam709 <113642641+pritam709@users.noreply.github.com>
2023-02-17 12:13:03 +0530

    Add files via upload

---
+++ javaScript.js
@@ -0,0 +1,13 @@
+let a="pritam"
+let b=1;
+console.log(a+b);
+console.log(typeof (a+b));
+const obj=
+{
+    name1:"pravesh",
+    section:1,
+    isprincipal:false 
+}
+obj['friend']= "dinna";
+obj['name1']="yadav";
+console.log(obj);
